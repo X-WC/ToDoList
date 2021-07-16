ad6b393411b703addb8bab972f1748ed955af740
congcong <862348941@qq.com>
2021-07-16 20:22:39 +0800

    使用vue-cli开发todolist完成

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="item" @click="handleDelete">{{ content }}</li>
+</template>
+
+<script>
+export default {
+  props: ["content", "index"],
+  methods: {
+    handleDelete: function () {
+      // console.log(this.index);
+      this.$emit("delete", this.index);
+    },
+  },
+};
+</script>
+
+
+
+<style scoped>
+/* scoped 属性可以保证样式为局部样式 */
+.item {
+  color: green;
+}
+</style>>
+
